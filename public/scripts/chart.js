5f4906e38624872fed09bef6526fc1a20e7bffd8
Nik Sofianos <nsofianos@hotmail.com>
2021-09-12 21:49:16 +0000

    create function for making bar elements, update drawBarChart to prepend bar elements based on data param

--- public/scripts/chart.js
+++ public/scripts/chart.js
@@ -1,18 +1,25 @@
 $(document).ready(function () {
   $("button").click(function () {
     $(".chart-container").empty();
-    $(".chart-container").prepend(drawBarChart());
+    $(".chart-container").prepend(drawBarChart(6));
   });
 });
+
+const createBarElement = () => {
+  const $bar = $(`
+    <div class="bar"></div>
+  `);
+  return $bar;
+};
 
 const drawBarChart = (data, options, element) => {
   console.log("drawn chart");
   const $barChart = $(`
   <div class="chart">
-    <div class="bar"></div>
-    <div class="bar"></div>
-    <div class="bar"></div>
   </div>
   `);
+  for (let i = 0; i < data; i++) {
+    $barChart.prepend(createBarElement());
+  }
   return $barChart;
 };
