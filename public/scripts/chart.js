b498ef85529c17d378a14eb34a764a7bc490c6cc
Nik Sofianos <nsofianos@hotmail.com>
2021-09-12 18:48:24 +0000

    fix linter, create main css file

--- public/scripts/chart.js
+++ public/scripts/chart.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
   $("button").click(function () {
     $("p").hide();
+    drawBarChart();
   });
 });
+
+const drawBarChart = () => {
+  console.log("drawn chart");
+};
